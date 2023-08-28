4f9d34d2d961cf28f87d44b326df218601bb30bb
Miunn <remcaulier@orange.fr>
2023-08-28 12:30:53 +0200

    On click in seeall page

---
+++ static/js/seeall-img.js
@@ -0,0 +1,5 @@
+Array.from(document.getElementsByClassName("list-img")).forEach((elmt) => {
+    elmt.addEventListener("click", (event) => {
+        window.open(event.target.src, 'Image', );
+    });
+});
